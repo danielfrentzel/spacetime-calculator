27ecfd860e9d4b5abb1710e9c38eeb3cd61f351d
Daniel Frentzel <daniel@Daniels-MacBook-Pro.local>
2021-01-29 21:37:56 -0700

    Implement flask server

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from hour_calculator import HourCalculator
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    time_input = request.form['time_input']
+    try:
+        hours = HourCalculator(time_input).calculate()
+        total = hours['$total']
+        del hours['$total']
+
+        # todo
+        # keep order
+        # display breaks
+
+        calculated_hours = []
+        for chg, hrs in hours.items():
+            calculated_hours.append([chg, hrs])
+
+        calculated_hours.append(['total', total])
+        print(calculated_hours)
+
+    except ValueError as e:
+        print(e)
+        calculated_hours = [['Invalid time input', '']]
+    return render_template('index.html', time_input=time_input, calculated_hours=calculated_hours)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='127.0.0.1', port='5000')
